dec0e7711fb5a16a24e5a2730d03d9ffe2ab3cc2
tatazoe <takashi.tazoe@gmail.com>
2022-12-12 06:48:31 +0900

    Degenerate Objects

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -2,14 +2,18 @@ package money;
 
 import org.example.money.Dollar;
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 
 }
