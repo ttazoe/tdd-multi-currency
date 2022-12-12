b6bc6653c2a9160194b678e9f3d597ef0d51f3d4
tatazoe <takashi.tazoe@gmail.com>
2022-12-13 05:17:49 +0900

    Hidden implementation

--- src/main/java/org/example/money/Money.java
+++ src/main/java/org/example/money/Money.java
@@ -1,11 +1,23 @@
 package org.example.money;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar (int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc (int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return this.amount == money.amount;
+        return this.amount == money.amount && this.getClass().equals(money.getClass());
     }
 
 }
