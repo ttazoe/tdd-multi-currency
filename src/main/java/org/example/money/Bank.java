21d497fd75f21e415ee106039cedead0570ec406
tatazoe <takashi.tazoe@gmail.com>
2022-12-16 04:52:26 +0900

    Test for learning and rgression test

--- src/main/java/org/example/money/Bank.java
+++ src/main/java/org/example/money/Bank.java
@@ -1,7 +1,24 @@
 package org.example.money;
 
+import java.util.Map;
+import java.util.HashMap;
+
+
 public class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
+
     public Money reduce(Expression source, String to){
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
+
+    public void addRate(String from, String to, int rate){
+        rates.put(new Pair(from, to), rate);
+    }
+
+    public int rate (String from, String to) {
+        if (from.equals(to)) return 1;
+        return this.rates.get(new Pair(from,to));
+    }
+
+
 }
--- src/main/java/org/example/money/Money.java
+++ src/main/java/org/example/money/Money.java
@@ -42,8 +42,8 @@ public class Money implements Expression {
         return new Sum(this, addend);
     }
 
-    public Money reduce(String to) {
-        return this;
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(this.currency, to);
+        return new Money(this.amount / rate, to);
     }
-
 }
